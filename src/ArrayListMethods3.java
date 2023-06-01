138d12c54236bed556af7436c130a1c856b1702f
Abdulaziz <abdusattorov2004@mail.ru>
2023-06-01 11:10:43 +0500

    sd

---
+++ src/ArrayListMethods3.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class ArrayListMethods3 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("Azura");
+        arrayList1.add("Kazim");
+        arrayList1.add("Nazim");
+        arrayList1.add("Azim");
+        System.out.println(arrayList1);
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        arrayList2.add("!!!");
+        arrayList2.add("???");
+        arrayList1.addAll(arrayList2);
+        System.out.println(arrayList1);
+
+    }}
